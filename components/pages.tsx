8bad876b6c4d701728342ee630bc122bee163a18
Shivansh Verma <shivanshverma082004@gmail.com>
2024-11-12 22:14:24 +0530

    navlinks updated

--- components/pages.tsx
+++ components/pages.tsx
@@ -1,11 +1,11 @@
-import { Home, Info, Calendar, Users, Mail } from "lucide-react";
+import { Home, Calendar, Users, Mail, User } from "lucide-react";
 
 export const getPages = () => {
   return [
     { name: "Home", href: "/", icon: Home },
-    { name: "About", href: "/about", icon: Info },
-    { name: "Events", href: "/events", icon: Calendar },
-    { name: "Team", href: "/team", icon: Users },
+    { name: "Swipe", href: "/matching", icon: Calendar },
+    { name: "My Matches", href: "/matches", icon: Users },
+    { name: "Profile", href: "/profile", icon: User },
     { name: "Contact", href: "/contact", icon: Mail },
   ];
 };
